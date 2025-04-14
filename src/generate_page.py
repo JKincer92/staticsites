1eacd52064e95b97f56024edb3a51f3e06be5efd
JKincer92 <cuckoosince92@gmail.com>
2025-04-14 04:04:21 -0700

    updated to docs

--- src/generate_page.py
+++ src/generate_page.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from markdown_blocks import markdown_to_html_node
 from extract_title import extract_title
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, 'r') as markdown_file:
         read_markdown = markdown_file.read()
@@ -16,20 +16,22 @@ def generate_page(from_path, template_path, dest_path):
     page_title = extract_title(read_markdown)
     final_content = read_template.replace('{{ Title }}', page_title)
     final_content = final_content.replace('{{ Content }}', html_string)
+    final_content = final_content.replace('href="/', f'href="{basepath}')
+    final_content = final_content.replace('src="/', f'src="{basepath}')
 
     with open(dest_path, 'w') as file:
         file.write(final_content)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
 
     for item in Path(dir_path_content).iterdir():
         if item.is_dir():
             dest_dir = Path(dest_dir_path) / item.name
             dest_dir.mkdir(parents=True, exist_ok=True)
-            generate_pages_recursive(item, template_path, dest_dir)
+            generate_pages_recursive(item, template_path, dest_dir, basepath)
         elif item.suffix == ".md":
             dest_file = Path(dest_dir_path) / (item.stem + ".html")
-            generate_page(item, template_path, dest_file)
+            generate_page(item, template_path, dest_file, basepath)
 
 
         
--- src/main.py
+++ src/main.py
@@ -3,21 +3,29 @@ from textnode import TextType
 from static_to_public import static_to_public
 from generate_page import generate_page, generate_pages_recursive
 import os
+import sys
 
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 static_dir = os.path.join(project_root, "static")
-public_dir = os.path.join(project_root, "public")
+docs_dir = os.path.join(project_root, "docs")
 content_dir = os.path.join(project_root, "content")
 
 def main():
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+
     test = TextNode("anchor", TextType.LINK , "website.com")
     print(test)
-    static_to_public(static_dir, public_dir)
+    static_to_public(static_dir, docs_dir)
 
     index_md_path = os.path.join(content_dir, "index.md")
     template_html_path = os.path.join(project_root, "template.html")
-    output_html_path = os.path.join(public_dir, "index.html")
-    generate_pages_recursive(content_dir, template_html_path, public_dir)
+    output_html_path = os.path.join(docs_dir, "index.html")
+    generate_pages_recursive(content_dir, template_html_path, docs_dir, basepath)
+
+
 
 
 main()
